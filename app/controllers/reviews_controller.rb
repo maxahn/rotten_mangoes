bb2b32ea93fdd5a08a255fca47af201e73223b28
maxahn <ahnmaximus@gmail.com>
2016-06-21 19:01:35 -0700

    using a before_filter in the reviews_controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,11 @@
 class ReviewsController < ApplicationController
+  before_filter :load_movie
+
   def new
-    @movie = Movie.find(params[:movie_id])
     @review = @movie.reviews.build
   end
 
   def create
-    @movie = Movie.find(params[:movie_id])
     @review = @movie.reviews.build(review_params)
     @review.user_id = current_user.id
 
@@ -17,7 +17,9 @@ class ReviewsController < ApplicationController
   end
 
   protected
-
+  def load_movies
+    @movie = Movie.find(params[:movie_id])
+  end
   def review_params
     params.require(:review).permit(:text, :rating_out_of_ten)
   end
