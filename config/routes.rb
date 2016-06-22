ab6a7540cf92d5efe8a05109796a1194c98976b0
maxahn <ahnmaximus@gmail.com>
2016-06-21 18:56:22 -0700

    generating reviews ontroller and reviews/new view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+
+  get 'reviews/create'
+
   get 'sessions/new'
 
   get 'sessions/create'
@@ -6,8 +10,9 @@ Rails.application.routes.draw do
   get 'users/new'
   get 'users/create'
 
-  resources :sessions, only: [:new, :create, :destroy]
-  resources :movies
+  resources :movies do 
+    resources :reviews, only: [:new, :create]
+  end
   resources :users, only: [:new, :create]
-
+  resources :sessions, only: [:new, :create, :destroy]
 end
