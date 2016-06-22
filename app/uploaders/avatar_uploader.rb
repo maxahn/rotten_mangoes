ef8d32722b9fd7674e59bebaa599f501f516dd7f
maxahn <ahnmaximus@gmail.com>
2016-06-22 16:37:50 -0700

    admin users page frame implemented

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -20,7 +20,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   end
 
   #version :thumb do
-  #  process :resize_to_fill => [160,160]
+  #  process :resize_to_fill => [200,200]
   #end
 
   def extension_white_list
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,36 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
+  namespace :admin do
+  get 'users/index'
+  end
 
+  namespace :admin do
+  get 'users/show'
+  end
+
+  namespace :admin do
+  get 'users/create'
+  end
+
+  namespace :admin do
+  get 'users/update'
+  end
+
+  namespace :admin do
+  get 'users/new'
+  end
+
+  namespace :admin do
+  get 'users/destroy'
+  end
+
+  namespace :admin do
+  get 'users/edit'
+  end
+
+  get 'reviews/new'
   get 'reviews/create'
 
   get 'sessions/new'
-
   get 'sessions/create'
 
   get 'users/new'
@@ -13,8 +39,13 @@ Rails.application.routes.draw do
   resources :movies do 
     resources :reviews, only: [:new, :create]
   end
+
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
 
+  namespace :admin do
+    resources :users 
+  end
   root to: 'movies#index'
+
 end
