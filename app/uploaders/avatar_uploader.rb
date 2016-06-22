b6fb17d09a40fee11a659c8be6cd5eb157b2af8a
maxahn <ahnmaximus@gmail.com>
2016-06-22 13:26:43 -0700

    image uploader is now working

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -19,9 +19,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  version :thumb do
-    process :resize_to_fill => [200,300]
-  end
+  #version :thumb do
+  #  process :resize_to_fill => [160,160]
+  #end
 
   def extension_white_list
     %w(jpg jpeg gif png)
